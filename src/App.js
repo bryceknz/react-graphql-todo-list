8551a197a07cfb794e9a5b900c01569361005c22
Bryce Kehoe <bryce@kehoe.co.nz>
2020-11-20 15:12:58 +0000

    First GraphQL query on the front-end! 🎉🙌

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+
 import './App.css'
 
 function App() {
+  const query = gql`
+    {
+      todos {
+        id
+        text
+        completed
+      }
+    }
+  `
+
   return (
     <div className="App">
-      <header className="App-header">Todo lol</header>
+      <header className="App-header">
+        <Query query={query}>
+          {({ loading, error, data }) => (
+            <>
+              {loading && <div>Fetching...</div>}
+              {error && <div>Error 😞</div>}
+              {data &&
+                data.todos.map((todo) => <p key={todo.id}>{todo.text}</p>)}
+            </>
+          )}
+        </Query>
+      </header>
     </div>
   )
 }
