e253c815a0c0daa3c1a335967c27a435863da236
Bryce Kehoe <bryce@kehoe.co.nz>
2020-11-20 16:30:01 +0000

    Changed data type of id to string

--- server/src/data.js
+++ server/src/data.js
@@ -1,8 +1,8 @@
 module.exports = {
   todos: [
-    { id: 0, text: 'Learn GraphQL', completed: false },
-    { id: 1, text: 'Seek feedback', completed: false },
-    { id: 2, text: 'Learn urql', completed: false },
-    { id: 3, text: 'Contribute to urql', completed: false },
+    { id: '0', text: 'Learn GraphQL', completed: false },
+    { id: '1', text: 'Seek feedback', completed: false },
+    { id: '2', text: 'Learn urql', completed: false },
+    { id: '3', text: 'Contribute to urql', completed: false },
   ],
 }
