0af4f9012212d6e45bdb6141fa7e7d33418a0e4d
Bryce Kehoe <bryce@kehoe.co.nz>
2020-11-23 09:11:07 +0000

    Update data

--- server/src/data.js
+++ server/src/data.js
@@ -1,6 +1,6 @@
 module.exports = {
   todos: [
-    { id: '0', text: 'Learn GraphQL', completed: false },
+    { id: '0', text: 'Learn GraphQL', completed: true },
     { id: '1', text: 'Seek feedback', completed: false },
     { id: '2', text: 'Learn urql', completed: false },
     { id: '3', text: 'Contribute to urql', completed: false },
