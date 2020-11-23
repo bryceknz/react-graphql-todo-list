1118fdc6ef881775c5c34421809c40f93aa956dd
Bryce Kehoe <bryce@kehoe.co.nz>
2020-11-23 09:20:30 +0000

    Added mutation to TodoListItem

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
 
 import './TodoListItem.css'
 
-function TodoListItem({ text, completed }) {
-  return <li className={completed && 'completed'}>{text}</li>
+function TodoListItem({ id, text, completed }) {
+  const mutation = gql`
+    mutation {
+      toggleComplete(id: ${id}) {
+        completed
+      }
+    }
+  `
+
+  return (
+    <Mutation mutation={mutation}>
+      {(toggleComplete) => (
+        <li className={completed ? 'completed' : ''} onClick={() => toggleComplete(id)}>
+          {text}
+        </li>
+      )}
+    </Mutation>
+  )
 }
 
 export default TodoListItem
